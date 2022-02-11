6b7bdcb200242133de0b040dc6276b6d3efe06e9
Siddhi Mankar <68636373+sid6602@users.noreply.github.com>
2022-02-12 01:06:45 +0530

    Create circles_using_inbuilt_functions.c

---
+++ circles_using_inbuilt_functions.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+ 
+int main(){
+   int gd = DETECT,gm;
+   int x ,y;
+   initgraph(&gd, &gm, "C:\\TC\\BGI");
+   
+   x = getmaxx()/2;
+   y = getmaxy()/2;
+ 
+  
+   setcolor(YELLOW);
+   circle(x, y, 0);
+   setcolor(WHITE);
+   circle(x, y, 30);
+   setcolor(GREEN);
+   circle(x, y, 60);
+   setcolor(YELLOW);
+   circle(x, y, 90);
+   setcolor(RED);
+   circle(x, y, 120);
+ 
+   getch();
+   closegraph();
+   return 0;
+}
