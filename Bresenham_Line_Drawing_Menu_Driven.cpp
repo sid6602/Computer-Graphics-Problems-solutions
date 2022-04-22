25420fc7cbee859db1d2423e2bd74787cc266993
Siddhi Mankar <68636373+sid6602@users.noreply.github.com>
2022-04-22 17:16:29 +0530

    Update Bresenham_Line_Drawing_Menu_Driven.cpp

--- Bresenham_Line_Drawing_Menu_Driven.cpp
+++ Bresenham_Line_Drawing_Menu_Driven.cpp
@@ -52,3 +52,57 @@ int main(){
                 y1=y1+yk;
             }
         break;
+
+      case 2:
+            //  Thick line
+            printf("\nEnter starting co-ordinates:");
+    scanf("%d%d",&x1,&y1);
+    printf("\nEnter ending co-ordinates:");
+    scanf("%d%d",&x2,&y2);
+            printf("\nEnter the required thickness: ");
+            scanf("%d",&thickness);
+            line (x1, y1, x2, y2);
+            if ((y2 - y1) / (x2 - x1) < 1)
+            {
+            wy = (thickness-1)*sqrt(pow((x2-x1),2)+pow((y2-y1),2))/(2*fabs(x2-x1));  for(i = 0; i < wy; i++);
+            {
+            line(x1, y1 - i, x2, y2 - i);
+            line(x1, y1 + i, x2, y2 + i);
+            }
+            }
+            else
+            {
+            wx = (thickness-1)*sqrt(pow((x2-x1),2)+pow((y2-y1),2))/(2*fabs(y2-y1));  for(i = 0; i < wx; i++);
+            {
+            line(x1 - i, y1, x2 - i, y2);  line(x1 + i, y1, x2 + i, y2);
+            }
+            }
+
+        break;
+        
+        case 3:
+        printf("\nEnter starting co-ordinates:");
+    scanf("%d%d",&x1,&y1);
+    printf("\nEnter ending co-ordinates:");
+    scanf("%d%d",&x2,&y2);
+            dx=x2-x1;
+            dy=y2-y1;  
+            if(abs(dx)>=abs(dy)){
+                len=abs(dx);
+            }else{
+                len=abs(dy);
+            }
+            
+            xk= dx/len;
+            yk= dy/len;
+            for( i=0; i<len;i++){
+                if(i%2==0){
+                putpixel(x1,y1,WHITE);
+                }
+                // delay(50);
+                x1=x1+xk;
+                y1=y1+yk;
+            }
+        break;
+      
+      
